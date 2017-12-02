7cf712b63c1107a07632498ed577c30996ed193d
Andy Chen <w5151381guy@gmail.com>
2017-12-02 23:18:04 +0800

    first version

--- dist/script/bundle.js
+++ dist/script/bundle.js
@@ -75,12 +75,20 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function rotateScreen() {
     $('div#container').toggleClass('rotated');
+    $('div.highcharts-container ').css('width', '100vh').css('height', '100vw');
+    svg = document.getElementsByTagName('svg')[0];
+    svg.setAttribute('width', '100vh');
+    svg.setAttribute('height', '100vw');
     $('.fa-arrows-alt').hide();
     $('.fa-compress').show();
 }
 
 function straightScreen() {
     $('div#container').removeClass('rotated');
+    $('div.highcharts-container ').css('width', '100%').css('height', '400px');
+    svg = document.getElementsByTagName('svg')[0];
+    svg.setAttribute('width', '100%');
+    svg.setAttribute('height', '400px');
     $('.fa-arrows-alt').show();
     $('.fa-compress').hide();
 }
--- src/script/index.js
+++ src/script/index.js
@@ -2,12 +2,24 @@ import view from './view'
 
 function rotateScreen() {
     $('div#container').toggleClass('rotated')
+    $('div.highcharts-container ')
+        .css('width', '100vh')
+        .css('height', '100vw')
+    svg = document.getElementsByTagName('svg')[0]
+    svg.setAttribute('width', '100vh')
+    svg.setAttribute('height', '100vw')
     $('.fa-arrows-alt').hide()
     $('.fa-compress').show()
 }
 
 function straightScreen() {
     $('div#container').removeClass('rotated')
+    $('div.highcharts-container ')
+    .css('width', '100%')
+    .css('height', '400px')
+    svg = document.getElementsByTagName('svg')[0]
+    svg.setAttribute('width', '100%')
+    svg.setAttribute('height', '400px')
     $('.fa-arrows-alt').show()
     $('.fa-compress').hide()
 }
